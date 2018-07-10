883ae0315e8237ade033380497cf0419900ad805
briankh <cyclode5@gmail.com>
2018-07-10 16:23:26 -0300

    Added Registered Classes functionality, updated welcomepage
    WelcomePage updated to logout and added registeredclasses activity to display classes currently taken by students.
    By Biswesh and Zac
    
    Co-Authored-By: Zac <zaco230@users.noreply.github.com>

--- app/src/main/java/com/example/zacco/universitycourseregistration/RegisteredClasses.java
+++ app/src/main/java/com/example/zacco/universitycourseregistration/RegisteredClasses.java
@@ -1,13 +1,65 @@
 package com.example.zacco.universitycourseregistration;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.app.ListActivity;
 
-public class RegisteredClasses extends AppCompatActivity {
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class RegisteredClasses extends ListActivity {
+
+    private FirebaseAuth auth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_registered_classes);
+        populateRegistered();
+    }
+
+    public void populateRegistered() {
+        auth = FirebaseAuth.getInstance();
+        Log.v("Method","populateRegistered Called");
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Students/"+auth.getUid()+"/Courses");
+        Log.v("Check", dbRef.getKey());
+        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map<String,Object> courseMap = (HashMap<String,Object>) dataSnapshot.getValue();
+                Log.w("check",""+ courseMap.get("Course1"));
+                getArray(courseMap);
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("DATASNAPSHOT", "Datasnapshot error");
+            }
+        });
+    }
+
+    public void getArray(Map<String, Object> map) {
+        String[] currentClasses = new String[map.size()];
+        int i = 0;
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            currentClasses[i] = (String) entry.getValue();
+            i++;
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, currentClasses);
+        setListAdapter(arrayAdapter);
     }
 }
--- app/src/main/java/com/example/zacco/universitycourseregistration/WelcomePage.java
+++ app/src/main/java/com/example/zacco/universitycourseregistration/WelcomePage.java
@@ -1,23 +1,75 @@
 package com.example.zacco.universitycourseregistration;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class WelcomePage extends AppCompatActivity {
+    private TextView welcomeText;
+    private FirebaseAuth auth;
+    private DatabaseReference dbRef;
+    private Button logoutBtn;
+    private Button activityRegistered;
+    private String fName;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_welcome_page);
+        auth = FirebaseAuth.getInstance();
+        dbRef = FirebaseDatabase.getInstance().getReference().child("Students");
+        dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange( DataSnapshot dataSnapshot) {
+                for (DataSnapshot students : dataSnapshot.getChildren()){
+                    Log.v("what",""+ students.getKey());
+                    Log.v("tmz", "" + students.child("First Name").getValue());
+                    String key = students.getKey();
+                    if(key.equals(auth.getUid())){
+                        fName = "" + students.child("First Name").getValue();
+                        welcomeText = (TextView) findViewById(R.id.Welcome);
+                        welcomeText.setText("Welcome " +fName+ " to The University of Maximegalon Course Registration!");
+                        Log.v("tmz", "" + students.child("First Name").getValue());
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                Log.d("check", "failed");
+            }
+        });
+
+        logoutBtn = (Button) findViewById(R.id.logout);
+        logoutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+                System.exit(0);
+            }
+        });
+
+        activityRegistered = (Button) findViewById(R.id.registeredclasses);
+        activityRegistered.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                goToRegisteredClasses();
+            }
+        });
     }
     /** Called when the user taps the button */
-    public void logout(View view) {
-        // Do something in response to button
-        Intent startNewActivity = new Intent(this, LoginActivity.class);
-        startActivity(startNewActivity);
-    }
-    public void goToRegisteredClasses(View view) {
+    public void goToRegisteredClasses() {
         // Do something in response to button
         Intent startNewActivity = new Intent(this, RegisteredClasses.class);
         startActivity(startNewActivity);
