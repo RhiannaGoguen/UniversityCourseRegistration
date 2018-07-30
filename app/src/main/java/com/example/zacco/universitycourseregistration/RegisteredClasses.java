025a7b205c4ba4d2df9423a84ab4e024a46e08c5
hassanAlbaghdadi <hasan_hamed94@yahoo.com>
2018-07-30 15:06:40 -0300

    Update RegisteredClasses.java

--- app/src/main/java/com/example/zacco/universitycourseregistration/RegisteredClasses.java
+++ app/src/main/java/com/example/zacco/universitycourseregistration/RegisteredClasses.java
@@ -1,86 +1,65 @@
 package com.example.zacco.universitycourseregistration;
 
-import android.content.Intent;
-import android.support.annotation.NonNull;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.app.ListActivity;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 import java.util.Map;
 
-public class WelcomePage extends AppCompatActivity {
-    private TextView welcomeText;
+public class RegisteredClasses extends ListActivity {
+
     private FirebaseAuth auth;
-    private DatabaseReference dbRef;
-    private Button logoutBtn;
-    private Button activityRegistered;
-    private String fName;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_welcome_page);
+        setContentView(R.layout.activity_registered_classes);
+        populateRegistered();
+    }
+
+    public void populateRegistered() {
         auth = FirebaseAuth.getInstance();
-        dbRef = FirebaseDatabase.getInstance().getReference("Students/"+auth.getUid());
+        Log.v("Method","populateRegistered Called");
+        DatabaseReference dbRef = FirebaseDatabase.getInstance().getReference("Students/"+auth.getUid()+"/Courses");
+        Log.v("Check", dbRef.getKey());
         dbRef.addListenerForSingleValueEvent(new ValueEventListener() {
+
             @Override
-            public void onDataChange( DataSnapshot dataSnapshot) {
-                Map<String,Object> studentMap = (HashMap<String,Object>) dataSnapshot.getValue();
-                fName = (String) studentMap.get("First Name");
-                welcomeText = (TextView) findViewById(R.id.Welcome);
-                welcomeText.setText("Welcome " +fName+ " to The University of Maximegalon Course Registration!");
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                Map<String,Object> courseMap = (HashMap<String,Object>) dataSnapshot.getValue();
+                Log.w("check",""+ courseMap.get("Course1"));
+                getArray(courseMap);
 
             }
 
             @Override
-            public void onCancelled(@NonNull DatabaseError databaseError) {
-                Log.d("check", "failed");
-            }
-        });
-
-        logoutBtn = (Button) findViewById(R.id.logout);
-        logoutBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-                System.exit(0);
-            }
-        });
-
-        activityRegistered = (Button) findViewById(R.id.registeredclasses);
-        activityRegistered.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                goToRegisteredClasses();
+            public void onCancelled(DatabaseError databaseError) {
+                Log.e("DATASNAPSHOT", "Datasnapshot error");
             }
         });
     }
-    /** Called when the user taps the button */
-    public void goToRegisteredClasses() {
-        // Do something in response to button
-        Intent startNewActivity = new Intent(this, RegisteredClasses.class);
-        startActivity(startNewActivity);
-    }
-    public void goToAcademicTimetable(View view) {
-        // Do something in response to button
-        Intent startNewActivity = new Intent(this, AcademicTimetable.class);
-        startActivity(startNewActivity);
-    }
-    public void goToSchedule(View view) {
-        // Do something in response to button
+
+    public void getArray(Map<String, Object> map) {
+        String[] currentClasses = new String[map.size()];
+        int i = 0;
+        for (Map.Entry<String, Object> entry : map.entrySet()) {
+            currentClasses[i] = (String) entry.getValue();
+            i++;
+        }
+        ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, currentClasses);
+        setListAdapter(arrayAdapter);
     }
 }
-
